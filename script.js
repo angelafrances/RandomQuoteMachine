64c91291df286920adf56c5762aec6f9857190a8
Angie Cunningham <angela.f.cunningham@gmail.com>
2017-11-20 03:12:37 +0000

    update code

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
+/* global $ */
+/* global twttr $ */
 var quotes = [
-    "If you want me to take a dump in a box, and mark it guaranteed I will... I've got spare time.",
+    "If you want me to take a dump in a box and mark it guaranteed, I will... I've got spare time.",
     "Brothers don't shake hands. Brothers gotta hug.",
     "Does this suit make me look fat?",
     "Are you talking?",
@@ -17,9 +19,28 @@ var quotes = [
     "Fat guy in a liiittle coat.  Fat guy in a little coooat.",
     "Richard, were you watching 'spanktrovision'?",
     "That's gonna leave a mark."
-    ]
+    ];
+
+var currentQuote = '';
 
 function newQuote() {
-    var randomNumber = Math.floor(Math.random() * (quotes.length));
-    document.getElementById('quoteDisplay').innerHTML = quotes[randomNumber];
+    currentQuote = quotes[Math.floor(Math.random() * quotes.length)];
+
+    $('#quoteDisplay').html(currentQuote);
+    $('#tweetQuote').attr('href', 'https://twitter.com/intent/tweet?text='+currentQuote).attr('target', '_blank');
+
 }
+
+$(function() {
+	$('#hitme').click(function() {
+		newQuote();
+	});
+});
+
+twttr.widgets.load();
+
+
+
+
+
+
